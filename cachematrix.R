31ceb0fb9ece5ef824d6ab570e70823e7be7e90d
nzaidel <njz07016@yahoo.com>
2015-05-22 10:07:34 -0400

    completed prog assign 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions save computation time by utilizing a previously computed matrix inverse if it exists
 
-## Write a short comment describing this function
+
+## This function produces a list of 4 functions.  
+## it is used to cache a value that can be call in other environments
+## set globally assigns the matrix to x.  
+## get returns x.
+## setinv globally assigns the matrix inverse to m
+## getinv returns m
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(minv) m <<- minv
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of a matrix x.
+## If the matrix had previously been calculated and stored in m
+## it will return the value of m rather than recalculating.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
+
+mymat<-matrix(sample(1:9,16,TRUE),4,4)
+checkmakematrix<-makeCacheMatrix(mymat) 
+cacheSolve(checkmakematrix) 
+mymat<-matrix(sample(1:9,36,TRUE),6,6)
+cacheSolve(checkmakematrix) 
+checkmakematrix$setinv(NULL)
+cacheSolve(checkmakematrix) 
+checkmakematrix$setinv(NULL)
+checkmakematrix<-makeCacheMatrix(mymat) 
+cacheSolve(checkmakematrix) 
+
